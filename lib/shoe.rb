b4325008c6d9cde513662b8eef1b4177538cce05
Walter <walter.clark24@yahoo.com>
2023-01-23 03:46:57 -0500

    commit tree a20b330e6409087a5bd3b3d1e8aab5279e01d07e

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,20 +1,12 @@
 # Make your shoe class here!
-require 'pry'
-
 class Shoe
-    attr_reader :brand, :color,:size, :material, :condition
-    attr_writer :brand, :color,:size, :material, :condition
-    def initialize(brand ="Nike")
+    attr_accessor :brand, :color, :size, :material, :condition
+    def initialize(brand = "Nike")
         @brand = brand
-
     end
 
     def cobble
-        #self and @ are used for the same things
     puts "Your shoe is as good as new!"
     self.condition = "new"
     end
-
 end
-
-
