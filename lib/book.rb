a2b4c18f97d73f47bdd93379a4cfd716e0da2a36
Walter <walter.clark24@yahoo.com>
2023-01-23 03:32:20 -0500

    messed

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,30 @@
+require 'pry'
 
+class Book
+   
+    attr_accessor :title
+    attr_accessor :author, :page_count, :genre
+  #always, have to initialize 
+    def initialize(title)
+        @title = title
+        @author = nil
+        @page_count = 0
+        @genre = nil
+    
+    end
 
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
+#     def title
+#         @title
+#     end
+    
+#     def title=(new_title)
+#         @title = new_title
+# end
+# end
+#book blueprint: title
+#b1 "i like to read"
+#Book.new("i like to read")
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,20 @@
 # Make your shoe class here!
+require 'pry'
+
+class Shoe
+    attr_reader :brand, :color,:size, :material, :condition
+    attr_writer :brand, :color,:size, :material, :condition
+    def initialize(brand ="Nike")
+        @brand = brand
+
+    end
+
+    def cobble
+        #self and @ are used for the same things
+    puts "Your shoe is as good as new!"
+    self.condition = "new"
+    end
+
+end
+
+
